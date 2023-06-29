c13c97b4505648d650be5ef330164e51ff8a6a35
Natan_VilasBoas <86916855+Natan200-2@users.noreply.github.com>
2023-06-29 17:53:21 -0300

    primeiro commit

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    public function index(){
+
+        $events = Event::all();
+
+        return view('welcome', ["events" => $events]);
+    }
+
+    public function create(){
+        return view('events.create');
+    }
+
+    public function product(){
+        $busca = request('search');
+
+        return view('products', ['busca' => $busca]);
+    }
+
+    public function product_test($id = null){
+        return view('product', ['id' => $id]);
+    }
+
+}
---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('tittle', 'Criar Evento')
+
+@section('content')
+
+<h1>Crie um Evento</h1>
+
+@endsection
